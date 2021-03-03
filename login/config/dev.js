6dcc385a66ecf2ab107e7d2dda5a357a8aff2f0e
Shreya Shree <shreya.shree2221@gmail.com>
2021-03-03 14:33:28 +0530

    Database setup

---
+++ login/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:'mongodb+srv://Shreya22:Mongo@123@cluster0.8plgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey:'SECRET'
+
+
+};
---
+++ login/config/prod.js
@@ -0,0 +1,5 @@
+module.exports ={
+    mongoURI: ProcessingInstruction.env.MONGO_URI,
+    secretOrKey: ProcessingInstruction.env.SECRET_OR_KEY
+
+};
