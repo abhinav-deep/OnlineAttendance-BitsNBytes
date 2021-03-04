d4ae3b11cdabac54fb0bec983d19007ba404f0f4
Mohit Patel <mohitpatel040102@gmail.com>
2021-03-04 20:33:40 +0530

    Backend Login/Signup

---
+++ onlineAttendance/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey' : '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb+srv://Shreya22:Mongo@123@cluster0.8plgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
